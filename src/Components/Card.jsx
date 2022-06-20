e2bf4dc2aa2488b41f70231ab1a727bd34f16d38
Nathan <jhond999@gmail.com>
2022-06-20 19:43:17 -0300

    update 2

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import imagen from "../../src/image.png"
-const Card=()=>{
+const Card=({pokemon,loading,infoPokemon})=>{
+    
     return(
         <>
-            <div className="card">
-                <h2>1</h2>
-                <img src={imagen} alt="" />
-                <h2>Charmander</h2>
-            </div>
+            {loading?<h1>Loading...</h1>:
+            pokemon.map((item,key)=>{
+                return(
+                    <>
+                        <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
+                            <h2>{item.id}</h2>
+                            <img src={item.sprites.front_default} alt="" />
+                            <h2>{item.name}</h2>
+                        </div>
+                    </>
+                )
+            })}
         </>
     )
 }
--- src/Components/PokeInfo.jsx
+++ src/Components/PokeInfo.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 
-const PokeInfo=()=>{
-    return(
+const PokeInfo = ({ data }) => {
+    console.log(data)
+    return (
         <>
-            <h1>Charmander</h1>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"/>
-            <div className="abilities">
-                <div className="group">
-                    <h2>blaze</h2>
-                </div>
-                <div className="group">
-                    <h2>solar-power</h2>
-                </div>
-            </div>
-            <div className="base-stat">
-                <h3>HP:30</h3>
-                <h3>Atack:52</h3>
-                <h3>Defense:43</h3>
-                <h3>Special-Attack:50</h3>
-                <h3>Speed</h3>
-            </div>
+            {(!data) ? "" : (
+                <>
+                    <h1>{data.name}</h1>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" />
+                    <div className="abilities">
+                        {
+                            data.abilities.map(poke=>{
+                                return(
+                                    <>
+                                        <div className="group">
+                                            <h2>{poke.ability.name}</h2>
+                                        </div>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="base-stat">
+                        {
+                            data.stats.map(poke=>{
+                                return(
+                                    <h3>{poke.stat.name}:{poke.base_stat}</h3>
+                                )
+                            })
+                        }
+                    </div>
+                </>
+            )}
         </>
     )
 }
